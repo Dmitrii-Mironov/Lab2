3ac8628e068e7144c7b97d5f5a2c296486d2fe73
Dmitriy <mirdim777@gmail.com>
2024-11-22 00:36:30 +0300

    def find_binaries_3()

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ def is_binary_multiple_3(binary_string):
         decimal_value = int(binary_string, 2)
         return decimal_value % 3 == 0
     return False
+
+def find_binaries_3(text):
+    binary_pattern = r'\b[01]+\b'
+    binaries = re.findall(binary_pattern, text)
+    # Оставляем только кратные 3
+    multiples_of_3 = [b for b in binaries if is_binary_multiple_3(b)]
+    return multiples_of_3
